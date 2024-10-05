416bb26b379ccd49d6ec1b681914092b2ad10b85
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-10-05 19:25:50 -0400

    Add bullet trails

--- project/splash/splash_screen.gd
+++ project/splash/splash_screen.gd
@@ -1,6 +1,14 @@
 extends Control
 
+func _ready() -> void:
+	if OS.has_feature("editor"):
+		_start_game()
+
+
+func _start_game() -> void:
+	get_tree().change_scene_to_packed(preload("res://world/world.tscn"))
+
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
-		get_tree().change_scene_to_packed(preload("res://world/world.tscn"))
+		_start_game()
